c6bb1aefb2574de20074c81bd439c37de6edd766
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2022-01-05 21:05:12 +0100

    day 1 part 2

--- Day1.py
+++ Day1.py
@@ -5,3 +5,19 @@ with open("Day1.txt") as file:
         data.append(int(line.strip()))
 
 print(sum(data))
+
+# part 2
+value = 0
+
+def day1Part2(value, data):
+    seen = {value}
+    while True:
+        for item in data:
+            value = value + item
+            if value not in seen:
+                seen.add(value)
+            else:
+                return value
+
+
+print(f"The value {day1Part2(0, data)} is seen for the second time first.")
